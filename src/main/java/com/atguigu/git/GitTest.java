05ae6c4894fc8bf4a6d6300727706199293d42ab
zht <409522735@qq.com>
2023-08-25 19:23:18 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author zhuhaitao
+ * @create 2023-08-25-18:44
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
