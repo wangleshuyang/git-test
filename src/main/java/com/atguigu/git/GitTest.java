fe2dc0f53531dc7cc4853545cbdb136131c6ec36
wangleshuyang <409522735@qq.com>
2023-08-28 10:19:19 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello master!");
         System.out.println("hello hot-fix!");
         System.out.println("push commit!");
+        System.out.println("pull commit!");
     }
 }
